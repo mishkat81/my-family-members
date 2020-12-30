81fdf184362384192f815417d2d181cf90ef26df
Mishkat Ahmed <72969563+mishkat81@users.noreply.github.com>
2020-12-31 01:55:12 +0600

    my family members
    Here i created a web app using html, CSS and JavaScript coding.

---
+++ js/main.js
@@ -0,0 +1,68 @@
+
+//working duration start
+
+   
+
+function calculate_age(x){
+           
+            
+    var birth_date = new Date(x);
+    var birth_date_day = birth_date.getDate();
+    var birth_date_month = birth_date.getMonth();
+    var birth_date_year = birth_date.getFullYear();
+
+    var toDay_date = new Date();
+    var toDay_date_day = toDay_date.getDate();
+    var toDay_date_month = toDay_date.getMonth();
+    var toDay_date_year = toDay_date.getFullYear();
+
+    var calculated_age = 0;
+    var calculated_month = 0;
+    var calculated_day = 0; 
+    var calculated_age = toDay_date_year - birth_date_year;
+    
+
+    if(toDay_date_day > birth_date_day){
+        calculated_day = toDay_date_day - birth_date_day;
+        calculated_age = toDay_date_year - birth_date_year;
+        calculated_month = toDay_date_month - birth_date_month;
+   }else if(toDay_date_day < birth_date_day){
+        calculated_day = (toDay_date_day + 30) - birth_date_day;
+        calculated_month = toDay_date_month - birth_date_month - 1;
+        calculated_age = toDay_date_year - birth_date_year - 1;
+   }else if(toDay_date_month > birth_date_month){
+        calculated_age = toDay_date_year - birth_date_year;
+        calculated_month = toDay_date_month - birth_date_month;
+   }else if(toDay_date_month < birth_date_month){
+        calculated_age = toDay_date_year - birth_date_year - 1;
+        calculated_month = (toDay_date_month + 12) - birth_date_month;
+   }else{
+    calculated_age = toDay_date_year - birth_date_year;
+   }
+
+    return ('AGE: YEAR: '+calculated_age+', MONTH: '+calculated_month+', DAY: '+calculated_day);
+
+}
+
+// setting date of birth by ID
+document.getElementById('safar_uddin').innerHTML = calculate_age("12/25/1950");
+document.getElementById('rani').innerHTML = calculate_age("");
+document.getElementById('mishkat').innerHTML = calculate_age("12/16/1981");
+document.getElementById('moriom-mishkat').innerHTML = calculate_age("01/01/1987");
+document.getElementById('maisha').innerHTML = calculate_age("12/25/2013");
+document.getElementById('anisha').innerHTML = calculate_age("06/24/2018");
+document.getElementById('kawsar').innerHTML = calculate_age("01/27/1992");
+document.getElementById('ashfak').innerHTML = calculate_age("06/12/1994");
+
+// document.getElementById('ashfak').innerHTML = calculate_age("06/12/1994");
+// document.getElementById('ashfak').innerHTML = calculate_age("06/12/1994");
+// document.getElementById('ashfak').innerHTML = calculate_age("06/12/1994");
+document.getElementById('iskander').innerHTML = calculate_age("01/01/1968");
+
+
+
+
+
+
+
+
